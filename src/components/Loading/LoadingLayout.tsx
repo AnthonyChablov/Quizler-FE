631bf9711b4bdc93c94fbb312db7bb0f44c2115b
Anthony Chablov <achablov@myseneca.ca>
2023-09-10 02:33:06 -0400

    udpate loading layout

--- src/components/Loading/LoadingLayout.tsx
+++ src/components/Loading/LoadingLayout.tsx
@@ -12,12 +12,15 @@ const LoadingLayout: React.FC<LoadingLayoutProps> = ({
   useCircularProgress,
 }) => {
   return (
-    <div className="bg-slate-200 min-h-screen mt-20">
+    <div className="bg-slate-200 min-h-screen flex items-center justify-center">
       <Container>
         <div className="p-4 md:flex md:justify-between md:space-x-4 lg:space-x-8">
           {useCircularProgress ? (
-            <div className="flex items-center justify-center">
-              <CircularProgress />
+            <div className="flex items-center justify-center flex-col">
+              <CircularProgress color="secondary" size={35} />
+              <p className="text-2xl text-purple-500 font-semibold mt-4">
+                Loading
+              </p>
             </div>
           ) : (
             <>
